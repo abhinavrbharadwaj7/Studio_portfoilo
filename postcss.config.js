9f56ed07a481e0dae772d20fd5192ac8be5b6a16
abhinavrbharadwaj7 <navabhi51852@gmail.com>
2025-03-26 22:32:50 +0530

    updated code

---
+++ postcss.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'slate-900': '#0f172a',
+        'teal-400': '#2dd4bf',
+      }
+    },
+  },
+  plugins: [],
+}
