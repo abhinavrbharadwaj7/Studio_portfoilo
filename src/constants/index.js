dce15d6cf8ef13a302d1c34f8f8f15c4186670f0
abhinavrbharadwaj7 <navabhi51852@gmail.com>
2025-03-26 22:36:22 +0530

    updated code

---
+++ src/constants/index.js
@@ -0,0 +1,18 @@
+export const navLinks = [
+    { id: 'projects', title: 'Projects' },
+    { id: 'commercials', title: 'Commercials' },
+    { id: 'skills', title: 'Skills' },
+    { id: 'contact', title: 'Contact' },
+  ];
+  
+  export const projects = [
+    {
+      id: 1,
+      title: 'Yathabava',
+      role: 'Cinematographer',
+      description: 'A bold narrative project showcasing visual storytelling',
+      link: '#',
+      image: '/images/yathabava.jpg'
+    },
+    // Add other projects
+  ];
